74dc67af0839eb400d2208da98df03f846d7e0a6
Alex Marchant <alexjmarchant@gmail.com>
2018-01-22 22:11:36 -0500

    First commit

---
+++ ReverseReactComponent.js
@@ -0,0 +1,96 @@
+const e = React.createElement
+
+const attributeNamesMap = {
+  'class': 'className',
+  'for': 'htmlFor',
+}
+
+class ReverseReactComponent extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.root = this.props.container.children[0]
+    // Save a copy of the original dom contents as our
+    // template, without this saved copy we would be parsing
+    // the output from react
+    this.rootClone = this.root.cloneNode(true)
+  }
+
+  parseNode(node) {
+    switch (node.nodeName) {
+      case '#text':
+        return this.parseTextNode(node)
+      default:
+        return this.parseElementNode(node)
+    }
+  }
+
+  nodeAttributes(node) {
+    // Run through the attributes and make them work
+    // width react
+    return Array.from(node.attributes)
+      .reduce((accumulator, attr) => (Object.assign(
+        accumulator,
+        this.parseAttribute(attr)
+      )), {})
+  }
+
+  parseAttribute(attr) {
+    let name = attr.name
+
+    // Map attributes to react frienly names
+    if (attributeNamesMap[name]) { name = attributeNamesMap[name] }
+
+    // Save original attr no matter what to match the dom
+    // when react renders 
+    const reactAttributes = {[name]: attr.value}
+
+    // Replace data-react attributes with camelcase
+    // versions and eval the value
+    if (name.search('data-react-') !== -1) {
+      name = name.replace('data-react-', '')
+      name = this.hypenToCamelCase(name)
+      reactAttributes[name] = eval(attr.value)
+    }
+
+    return reactAttributes
+  }
+
+  hypenToCamelCase(str) {
+    return str.replace(/-([a-z])/g, (g) => g[1].toUpperCase())
+  }
+
+  parseChildren(node) {
+    return Array
+      .from(node.childNodes)
+      .map((child) => this.parseNode(child)) 
+  }
+
+  parseTextNode(node) {
+    // Split up text nodes into an array by curly braces
+    const parts = node.wholeText.split(/({.*?})/)
+
+    // Replace curly brace content with eval'ed results
+    return parts
+      .map((part) => {
+        const match = part.match(/{(.*?)}/)
+        if (match) {
+          const exp = match[1]
+          return eval(exp)
+        }
+        return part
+      })
+  }
+
+  parseElementNode(node) {
+    return e(
+      node.tagName.toLowerCase(),
+      this.nodeAttributes(node),
+      ...this.parseChildren(node)
+    )
+  }
+
+  render() {
+    return this.parseNode(this.rootClone)
+  }
+}
---
+++ examples/Form.js
@@ -0,0 +1,41 @@
+class Form extends ReverseReactComponent {
+  constructor(props) {
+    super(props)
+
+    this.defaultMessage = 'This is a fake form, its not making any network calls.'
+
+    this.state = {
+      username: '',
+      password: '',
+      sending: false,
+      message: this.defaultMessage,
+    }
+
+    this.submit = this.submit.bind(this)
+    this.inputChange = this.inputChange.bind(this)
+  }
+
+  inputChange(event) {
+    this.setState({
+      [event.currentTarget.name]: event.currentTarget.value,
+    })
+  }
+
+  submit(event) {
+    event.preventDefault()
+    this.setState({
+      sending: true,
+      message: this.defaultMessage,
+    })
+    setTimeout(() => {
+      this.setState({
+        sending: false,
+        message: 'Form sent successfully!',
+      })
+    }, 1500)
+  }
+}
+
+const container = document.getElementById('container')
+const element = React.createElement(Form, {container: container})
+ReactDOM.hydrate(element, container)
